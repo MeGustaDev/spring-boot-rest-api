e195abc407d1d8203c9f586ae64a2e4d4efe73ac
MeGustaDev <gustavoguimaraesreis@gmail.com>
2023-12-12 12:27:43 -0300

    project init

---
+++ src/test/java/com/gustavo/restapi/RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavo.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
